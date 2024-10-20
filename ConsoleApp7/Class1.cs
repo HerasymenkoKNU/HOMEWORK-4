acfc3bff49c1866c1969374a7dd36e28d6a60463
bgera <bgera@USER>
2024-10-20 18:46:48 +0300

    Add project files.

---
+++ ConsoleApp7/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+public interface IGeographicalObject
+{
+    void Information();
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        River river = new River
+        {
+            X = 23.4872,
+            Y = 21.849,
+            Name = "Some river",
+            Description = "Some descriprion of river",
+            Speed = 1000000,
+            Length = 132320801
+        };
+
+        river.Information();
+
+
+        Mountain mountain = new Mountain
+        {
+            X = 197.79230,
+            Y = 8372.32070,
+            Name = "Some name",
+            Description = "Some description of mountain",
+            Peak = 9749729711
+        };
+
+        mountain.Information();
+
+        Console.ReadKey();
+    }
+}
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+public abstract class GeographicalObject
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+
+    public virtual void Information()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Info: {Description}");
+        Console.WriteLine($"Cords: ({X}, {Y})");
+    }
+}
+
+
+public class River : GeographicalObject
+{
+    public double Speed { get; set; }
+    public double Length { get; set; }
+
+    public override void Information()
+    {
+        base.Information();
+
+        Console.WriteLine($"Speed of flow: {Speed} см/с");
+        Console.WriteLine($"Total length: {Length} ");
+
+    }
+}
+
+public class Mountain : GeographicalObject
+{
+    public double Peak { get; set; }
+
+    public override void Information()
+    {
+        base.Information();
+        Console.WriteLine($"Peak: {Peak}");
+    }
+}
+
